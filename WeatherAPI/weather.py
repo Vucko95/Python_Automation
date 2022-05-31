46c921a044b5a11827c6758050123c0a9bac962f
Ronald Suplina <tyger995@gmail.com>
2022-05-31 15:54:50 +0100

    v2

--- WeatherAPI/weather.py
+++ WeatherAPI/weather.py
@@ -5,19 +5,17 @@ r = requests.get(
 content = r.json()
 with open('./RestApi/weather.json', 'w', encoding='utf-8') as f:
     json.dump(content, f, ensure_ascii=False, indent=4)
-# weather = content['weather']
-# print(weather)
-# print(content)
 
 
 def check_city_temp(city_name, api_key='983e60a50ff45ccd9208c225c13db7b2'):
     city = requests.get(
         f'https://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={api_key}')
     city_content = city.json()
-    with open('./RestApi/city.json', 'w', encoding='utf-8') as f:
+    with open(f'./RestApi/{city_name}.json', 'w', encoding='utf-8') as f:
         json.dump(city_content, f, ensure_ascii=False, indent=4)
     return city_content
 
 
-check_city_temp(city_name='Dublin')
-# // todo users inputs City name and it saves as that name of city
+print("Please input City Name")
+city_name = input()
+check_city_temp(city_name)
