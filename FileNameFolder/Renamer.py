9f8d50260e5d5037d73923cfdbcf76b934fe83d0
Ronald Suplina <tyger995@gmail.com>
2022-06-01 21:24:23 +0100

    Explained Renamer

--- FileNameFolder/Renamer.py
+++ FileNameFolder/Renamer.py
@@ -6,7 +6,7 @@ for path in file_paths:
     if path.is_file():
         parent_folder = path.parts[-2]
         # print(parent_folder)
-        new_file_name = parent_folder + '-' + path.name
+        new_file_name = parent_folder + '-' + path.name  # STRING OBJECT
         # print(new_file_name)
-        new_file_path = path.with_name(new_file_name)
+        new_file_path = path.with_name(new_file_name)  # new PATH OBJECT
         path.rename(new_file_path)
